11a4f04ebc0ea8323980dd64547f7fe00dab8f18
17ankish <bansal.ankish1@gmail.com>
2019-10-04 15:55:58 +0530

    Update stacking function for SVM and printing util

--- src/ml_models/stacking.py
+++ src/ml_models/stacking.py
@@ -1,11 +1,10 @@
+
 import warnings
 warnings.filterwarnings("ignore")
 
 from sklearn.model_selection import KFold, StratifiedKFold
 from sklearn.metrics import roc_auc_score, f1_score
 from sklearn.metrics import classification_report as cr
-from catboost import Pool, CatBoostClassifier
-import lightgbm as lgb
 import xgboost as xgb
 
 import numpy as np
@@ -23,42 +22,8 @@ from sklearn.svm import SVR
 from sklearn.linear_model import RidgeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.svm import SVC
 
-
-def cat_model(X_train, y_train, X_valid, y_valid, features, X_test, 
-              num_round=10000, depth=4):
-    param = {
-        'loss_function'         : "Logloss", 
-        'eval_metric'           : "AUC",
-        'random_strength'       : 3.8,#1.5,
-        'border_count'          : 128,
-        'scale_pos_weight'      : 3.507,
-        'depth'                 : depth, 
-        'early_stopping_rounds' : 50,
-        'random_seed'           : 1337,
-        'task_type'             : 'CPU', 
-#         'subsample'             = 0.7, 
-        'iterations'            : 10000, 
-        'learning_rate'         : 0.05,#0.09,
-        'thread_count'          : 4,
-        'l2_leaf_reg'           : 3.076,
-    }
-    param['iterations'] = num_round
-    
-    _train = Pool(X_train[features], label=y_train)
-    _valid = Pool(X_valid[features], label=y_valid)
-
-    watchlist = [_train, _valid]
-    clf = CatBoostClassifier(**param)
-    clf.fit(_train, 
-            eval_set=watchlist, 
-            verbose=0,
-            use_best_model=True)
-        
-    oof  = clf.predict_proba(X_valid[features])[:,1]
-    test_pred  = clf.predict_proba(X_test[features])[:,1]
-    
-    return oof, test_pred
 
 def xgb_model(X_train, y_train, X_valid, y_valid, features, X_test, 
               num_round=10000, depth=4):
@@ -108,7 +73,7 @@ def passive_agg_model(X_train, y_train, X_valid, y_valid, X_test,
 
     clf = PassiveAggressiveClassifier(
             C=alpha, fit_intercept=True, max_iter=None, tol=None, 
-            early_stopping=False, validation_fraction=0.2, n_iter_no_change=5, 
+#             early_stopping=False, validation_fraction=0.2, n_iter_no_change=5, 
             shuffle=True, verbose=0, n_jobs=-1, random_state=1234, loss='squared_hinge',
             class_weight=balance, average=False, n_iter=500)
     
@@ -120,22 +85,22 @@ def passive_agg_model(X_train, y_train, X_valid, y_valid, X_test,
     return oof, test_pred
 
 def logreg_model(X_train, y_train, X_valid, y_valid, X_test, 
-                 alpha=1, balance='balanced'):
+                 alpha=500, balance='balanced'):
     
     clf = LogisticRegression(
             penalty='l2', dual=False, C=alpha, fit_intercept=True, 
             intercept_scaling=1, class_weight=balance, random_state=1234, 
-            max_iter=100, multi_class='warn', verbose=0, n_jobs=-1)
+            max_iter=100, verbose=0, n_jobs=-1)
     
     means = X_train.mean()
     clf.fit(X_train.fillna(means), y_train)
-    oof  = clf.predict_proba(X_valid.fillna(means))[:,0]
-    test_pred = clf.predict_proba(X_test.fillna(means))[:,0]
+    oof  = clf.predict_proba(X_valid.fillna(means))[:,1]
+    test_pred = clf.predict_proba(X_test.fillna(means))[:,1]
 
     return oof, test_pred
 
 def ridge_model(X_train, y_train, X_valid, y_valid, X_test, 
-                 alpha=1, balance='balanced'):
+                 alpha=0.01, balance='balanced'):
     
     clf = RidgeClassifier(
         alpha=alpha, fit_intercept=True, normalize=True, 
@@ -170,16 +135,17 @@ def stacking(train_df, target, test_df, split=4, depth=4, print_=False):
     f1s = []
 ##########################
     for fold_, (train_index, valid_index) in enumerate(folds.split(train_df, target)):
-        if print_: print("Fold {}".format(fold_), end=" ")
+        if print_: print("Fold {}".format(fold_), end=" \t\t")
     
         y_train, y_valid = target.iloc[train_index], target.iloc[valid_index]
         X_train, X_valid = train_df.iloc[train_index,:], train_df.iloc[valid_index,:]
         features = X_train.columns
-        if print_: print(X_train.shape, X_valid.shape)
+#         if print_: print(X_train.shape, X_valid.shape)
         
         num_rounds = 10000
+        ####################################
         # cat model
-        #################################
+        ####################################
         oof, test_pred = cat_model(
             X_train, y_train, 
             X_valid, y_valid, 
@@ -191,15 +157,11 @@ def stacking(train_df, target, test_df, split=4, depth=4, print_=False):
         oof_cat[valid_index] = oof
         pred_cat[:,fold_] = test_pred
         
-        
         y_bin = [1. if y_cont > 0.5 else 0. for y_cont in oof]
-        score = f1_score(y_valid, y_bin, average='weighted')
-        if print_: print(" cat f1 = ", score )
-        if print_: print("="*60)
+        cat_score = f1_score(y_valid, y_bin, average='weighted')
         gc.collect()
-        f1s.append(score)
+
         #################################
-        
         # xgb model
         #################################
         oof, test_pred = xgb_model(
@@ -214,13 +176,13 @@ def stacking(train_df, target, test_df, split=4, depth=4, print_=False):
         pred_xgb[:,fold_] = test_pred
         
         y_bin = [1. if y_cont > 0.5 else 0. for y_cont in oof]
-        score = f1_score(y_valid, y_bin, average='weighted')
-        if print_: print(" xgb f1 = ", score )
-        if print_: print("="*60)
+        xgb_score = f1_score(y_valid, y_bin, average='weighted')
+#         if print_: print("="*60)
+        f1s.append(xgb_score)
+
         ####################################
-        
         # passive regressive
-        #################################
+        ####################################
         oof, test_pred = passive_agg_model(
             X_train, y_train, 
             X_valid, y_valid, 
@@ -231,13 +193,11 @@ def stacking(train_df, target, test_df, split=4, depth=4, print_=False):
         pred_pas[:,fold_] = test_pred
         
         y_bin = [1. if y_cont > 0.5 else 0. for y_cont in oof]
-        score = f1_score(y_valid, y_bin, average='weighted')
-        if print_: print( " pass-agg f1 = ", score )
-        if print_: print("="*60)
+        pas_score = f1_score(y_valid, y_bin, average='weighted')
+
         ####################################
-        
         # logistic regression
-        #################################
+        ####################################
         oof, test_pred = logreg_model(
             X_train, y_train, 
             X_valid, y_valid, 
@@ -248,13 +208,11 @@ def stacking(train_df, target, test_df, split=4, depth=4, print_=False):
         pred_log[:,fold_] = test_pred
         
         y_bin = [1. if y_cont > 0.5 else 0. for y_cont in oof]
-        score = f1_score(y_valid, y_bin, average='weighted')
-        if print_: print( " logreg f1 = ", score )
-        if print_: print("="*60)
+        log_score = f1_score(y_valid, y_bin, average='weighted')
+
         ####################################
-        
         # ridge regression
-        #################################
+        ####################################
         oof, test_pred = ridge_model(
             X_train, y_train, 
             X_valid, y_valid, 
@@ -265,10 +223,14 @@ def stacking(train_df, target, test_df, split=4, depth=4, print_=False):
         pred_rid[:,fold_] = test_pred
         
         y_bin = [1. if y_cont > 0.5 else 0. for y_cont in oof]
-        score = f1_score(y_valid, y_bin, average='weighted')
-        if print_: print( " logreg f1 = ", score )
-        if print_: print("="*60)
+        rid_score = f1_score(y_valid, y_bin, average='weighted')
         ####################################
+        
+        if print_:
+            print("CAT: {:.3f} \t XGB: {:.3f} \t PASS-AGG: {:.3f} \t LOG-REG: {:.3f} \t RIDGE: {:.3f}".format(
+                cat_score, xgb_score, pas_score, log_score, rid_score
+            ))
+
         
     print("="*60)
     print( "  mean-f1 = ", np.mean(f1s))
