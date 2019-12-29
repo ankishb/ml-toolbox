ecf879a03f827371518defe21e6783fd1947f4eb
ankishb <bansal.ankish1@gmail.com>
2019-12-29 17:49:16 +0530

    Fix data on the fly script

---
+++ src/deep_ml/data_on_fly.py
@@ -0,0 +1,180 @@
+
+#######################################
+####        Data on the fly        ####
+#######################################
+
+import numpy as np
+import keras
+
+class DataGenerator(keras.utils.Sequence):
+    """Generates data for Keras
+    
+    References:
+        https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+        By Afshine Amidi and Shervine Amidi
+    """
+    def __init__(self, list_IDs, labels, batch_size=32, dim=(32,32,32), n_channels=1,
+                 n_classes=10, shuffle=True):
+        'Initialization'
+        self.dim = dim
+        self.batch_size = batch_size
+        self.labels = labels
+        self.list_IDs = list_IDs
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(list_IDs_temp)
+
+        return X, y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_temp):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        y = np.empty((self.batch_size), dtype=int)
+
+        # Generate data
+        for i, ID in enumerate(list_IDs_temp):
+            # Store sample
+            X[i,] = np.load('data/' + ID + '.npy')
+
+            # Store class
+            y[i] = self.labels[ID]
+
+        return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
+
+
+#######################################
+####   Data on the fly(flexible)   ####
+#######################################
+
+class DataGenerator(tensorflow.keras.utils.Sequence):
+    """Generates data for Keras
+    
+    References:
+        https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+        By Afshine Amidi and Shervine Amidi
+    """
+    def __init__(self, folder, filenames, pneumonia_locations=None, batch_size=32, image_size=256, shuffle=True, augment=False, predict=False):
+        self.folder = folder
+        self.filenames = filenames
+        self.pneumonia_locations = pneumonia_locations
+        self.batch_size = batch_size
+        self.image_size = image_size
+        self.shuffle = shuffle
+        self.augment = augment
+        self.predict = predict
+        self.on_epoch_end()
+        
+    def __load__(self, filename):
+        # load dicom file as numpy array
+        img = pydicom.dcmread(os.path.join(self.folder, filename)).pixel_array
+        # create empty mask
+        msk = np.zeros(img.shape)
+        # get filename without extension
+        filename = filename.split('.')[0]
+        # if image contains pneumonia
+        if filename in self.pneumonia_locations:
+            # loop through pneumonia
+            for location in self.pneumonia_locations[filename]:
+                # add 1's at the location of the pneumonia
+                x, y, w, h = location
+                msk[y:y+h, x:x+w] = 1
+        # resize both image and mask
+        img = resize(img, (self.image_size, self.image_size), mode='reflect')
+        msk = resize(msk, (self.image_size, self.image_size), mode='reflect') > 0.5
+        # if augment then horizontal flip half the time
+        if self.augment and random.random() > 0.5:
+            img = np.fliplr(img)
+            msk = np.fliplr(msk)
+        # add trailing channel dimension
+        img = np.expand_dims(img, -1)
+        msk = np.expand_dims(msk, -1)
+        return img, msk
+    
+    def __loadpredict__(self, filename):
+        # load dicom file as numpy array
+        img = pydicom.dcmread(os.path.join(self.folder, filename)).pixel_array
+        # resize image
+        img = resize(img, (self.image_size, self.image_size), mode='reflect')
+        # add trailing channel dimension
+        img = np.expand_dims(img, -1)
+        return img
+        
+    def __getitem__(self, index):
+        # select batch
+        filenames = self.filenames[index*self.batch_size:(index+1)*self.batch_size]
+        # predict mode: return images and filenames
+        if self.predict:
+            # load files
+            imgs = [self.__loadpredict__(filename) for filename in filenames]
+            # create numpy batch
+            imgs = np.array(imgs)
+            return imgs, filenames
+        # train mode: return images and masks
+        else:
+            # load files
+            items = [self.__load__(filename) for filename in filenames]
+            # unzip images and masks
+            imgs, msks = zip(*items)
+            # create numpy batch
+            imgs = np.array(imgs)
+            msks = np.array(msks)
+            return imgs, msks
+        
+    def on_epoch_end(self):
+        if self.shuffle:
+            random.shuffle(self.filenames)
+        
+    def __len__(self):
+        if self.predict:
+            # return everything
+            return int(np.ceil(len(self.filenames) / self.batch_size))
+        else:
+            # return full batches only
+            return int(len(self.filenames) / self.batch_size)
+
+
+
+
+import numpy as np
+
+# DefineParameters
+params = {}
+
+# Datasets
+partition = # IDs
+labels = # Labels
+
+# Generators
+training_generator   = DataGenerator(partition['train'], labels, **params)
+validation_generator = DataGenerator(partition['validation'], labels, **params)
+
+model.fit_generator(
+    generator=training_generator,
+    validation_data=validation_generator,
+    use_multiprocessing=True,
+    workers=6
+)
